e6feab059e122342f0f1b3e3b1148a3fbe9739bc
RimiD162 <rimid341@gmail.com>
2026-06-22 07:35:25 +0530

    implement home page layout with navigation, hero section, and categories

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 8080;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+app.get('/home/hotel',(req,res)=>{
+  res.send("This is all about hotel");
+})
+app.get('/home/Rentals',(req,res)=>{
+  res.send("this is all about rent")
+})
+app.get('/home/Lodges',(req,res)=>{
+  res.send("this is all about lodges")
+})
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+});
